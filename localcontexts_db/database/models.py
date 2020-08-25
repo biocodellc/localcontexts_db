a5700a0ceef892c7fde2520225fdef3a7ced7f73
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2020-08-25 16:21:11 -0700

    db setup

--- localcontexts_db/database/models.py
+++ localcontexts_db/database/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+#create your models here.
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=80)
+    age = models.IntegerField()
--- localcontexts_db/localcontexts_db/settings.py
+++ localcontexts_db/localcontexts_db/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'database',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'localcontexts_db.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'localcontextsdb',
+        'USER': 'postgres',
+        'PASSWORD': 'sashameatloaf',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
