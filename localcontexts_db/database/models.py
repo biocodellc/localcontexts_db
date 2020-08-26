c598c9beeef833a193c50c837c908572bf8a3125
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2020-08-26 10:32:53 -0700

    add database models

--- localcontexts_db/database/models.py
+++ localcontexts_db/database/models.py
@@ -1,10 +1,42 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
+class Users(models.Model):
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    email = models.CharField(max_length=80)
+    phone = models.CharField(max_length=80)
+    affiliation = models.CharField(max_length=80)
+    bio = models.CharField(max_length=150)
+    # date_created = 
+    # date_modified =
 
-#create your models here.
+class UserInstitution(models.Model):
+    users_id = models.IntegerField()
+    # institution_id =
+    admin = models.BooleanField()
+    manager = models.BooleanField()
+    # governance = 
+    # team =
 
-class Teacher(models.Model):
+class UserCommunity(models.Model):
+    users_id = models.IntegerField()
+    # institution_id = 
+    admin = models.BooleanField()
+    manager = models.BooleanField()
+    board = models.BooleanField()
+    # member =
+
+class Institutions(models.Model):
     name = models.CharField(max_length=80)
-    age = models.IntegerField()
+    code = models.CharField(max_length=80)
+    address = models.CharField(max_length=80)
+    contact_name = models.CharField(max_length=80)
+    contact_email = models.CharField(max_length=80)
+
+class Community(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=80)
+    address = models.CharField(max_length=80)
+    contact_name = models.CharField(max_length=80)
+    contact_email = models.CharField(max_length=80)
+
