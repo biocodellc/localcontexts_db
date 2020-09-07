29a31998ddbe8851feb1528bb47bc98676f68fda
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2020-09-07 13:10:30 -0700

    add admin page, edit data tables, add data tables to admin page

--- localcontexts_db/database/admin.py
+++ localcontexts_db/database/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from database.models import UserInstitution, UserCommunity, Institutions, Community
 
 # Register your models here.
+admin.site.register(UserInstitution)
+admin.site.register(UserCommunity)
+admin.site.register(Institutions)
+admin.site.register(Community)
--- localcontexts_db/database/models.py
+++ localcontexts_db/database/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Users(models.Model):
     first_name = models.CharField(max_length=80)
     last_name = models.CharField(max_length=80)
-    email = models.CharField(max_length=80)
+    email = models.EmailField(max_length=254)
     phone = models.CharField(max_length=80)
     affiliation = models.CharField(max_length=80)
     bio = models.CharField(max_length=150)
@@ -12,7 +12,7 @@ class Users(models.Model):
 
 class UserInstitution(models.Model):
     users_id = models.IntegerField()
-    # institution_id =
+    institution_id = models.CharField(max_length=100)
     admin = models.BooleanField()
     manager = models.BooleanField()
     governance = models.BooleanField()
@@ -20,7 +20,7 @@ class UserInstitution(models.Model):
 
 class UserCommunity(models.Model):
     users_id = models.IntegerField()
-    # institution_id = 
+    institution_id = models.CharField(max_length=100) 
     admin = models.BooleanField()
     manager = models.BooleanField()
     board = models.BooleanField()
@@ -31,12 +31,12 @@ class Institutions(models.Model):
     code = models.CharField(max_length=80)
     address = models.CharField(max_length=80)
     contact_name = models.CharField(max_length=80)
-    contact_email = models.CharField(max_length=80)
+    contact_email = models.EmailField(max_length=254)
 
 class Community(models.Model):
     name = models.CharField(max_length=80)
     code = models.CharField(max_length=80)
     address = models.CharField(max_length=80)
     contact_name = models.CharField(max_length=80)
-    contact_email = models.CharField(max_length=80)
+    contact_email = models.EmailField(max_length=254)
 
