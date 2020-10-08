ba2935f2eb0657ca7dafc2b4f75dcca00d93a05b
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2020-10-08 12:21:21 -0700

    create register page, link all index.html buttons to it

--- localcontexts/apps/accounts/views.py
+++ localcontexts/apps/accounts/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home_screen_view(request):
-    return render(request, "accounts/home.html", {})
 
-def register_screen_view(request):
+def register(request):
     return render(request, "accounts/register.html")
--- localcontexts/apps/pages/views.py
+++ localcontexts/apps/pages/views.py
@@ -7,3 +7,6 @@ def index(request):
 
 def about(request):
     return render(request, 'pages/about.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
