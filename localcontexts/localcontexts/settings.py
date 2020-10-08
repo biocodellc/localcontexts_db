4466809090349e36bde9319b660c65213906fab9
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2020-10-08 10:48:44 -0700

    modified templates, set up new app for static pages, added routes to about/ & accounts/

--- localcontexts/localcontexts/settings.py
+++ localcontexts/localcontexts/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = ['localhost','127.0.0.1']
 # Application definition
 INSTALLED_APPS = [
     'apps.accounts',
+    'apps.pages',
     'django_countries',
     'django.contrib.admin',
     'django.contrib.auth',
--- localcontexts/localcontexts/urls.py
+++ localcontexts/localcontexts/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from apps.accounts.views import home_screen_view
 
 
 urlpatterns = [
+    path('', include('apps.pages.urls')),
     path('admin/', admin.site.urls),
-    path('', home_screen_view, name="home"),
+    path('accounts/', home_screen_view, name="home"),
 ]
