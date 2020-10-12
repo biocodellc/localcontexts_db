310e9848bc231439bb771848885f4fde93ceb460
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2020-10-12 12:55:36 -0700

    add warning alerts at register, start registration form data capture

--- localcontexts/apps/accounts/views.py
+++ localcontexts/apps/accounts/views.py
@@ -1,12 +1,51 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from apps.accounts.models import Account
 
 # Create your views here.
 
 def register(request):
-    return render(request, "accounts/register.html")
+    if request.method == 'POST':
+        #Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        #Check for password match
+        if password == password2:
+            #Check username
+            if Account.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
+                return redirect('register')
+            else:
+                if Account.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is being used')
+                    return redirect('register')
+                else:
+                    # If this works, create user
+                    user = Account.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
+                    #Login after register
+                    # auth.login(request, user)
+                    # messages.success(request, 'User is now logged in')
+                    # return redirect('index')
+                    user.save()
+                    messages.success(request, 'You are registered')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
+    else:
+        return render(request, "accounts/register.html")
 
 def login(request):
-    return render(request, "accounts/login.html")
+    if request.method == 'POST':
+        print('logged in')
+        return redirect('index')
+    else:
+        return render(request, "accounts/login.html")
 
 def logout(request):
     return redirect('index')
