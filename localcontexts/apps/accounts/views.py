5736e3b1d632b334ac229b547aeb3fdba002b4d1
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2020-10-13 13:23:13 -0700

    add user login

--- localcontexts/apps/accounts/views.py
+++ localcontexts/apps/accounts/views.py
@@ -16,16 +16,17 @@ def register(request):
 
         #Check for password match
         if password == password2:
-            #Check username
+            #Check that username exists
             if Account.objects.filter(username=username).exists():
                 messages.error(request, 'That username is taken')
                 return redirect('register')
             else:
+                #Check that email exists
                 if Account.objects.filter(email=email).exists():
                     messages.error(request, 'That email is being used')
                     return redirect('register')
                 else:
-                    # If this works, create user
+                    # If data unique, create user
                     user = Account.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)
                     
                     # Use the following if you want to Login after register
@@ -43,8 +44,19 @@ def register(request):
 
 def login(request):
     if request.method == 'POST':
-        print('logged in')
-        return redirect('index')
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        # If user is found, log in the user.
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid Credentials')
+            return redirect('login')
     else:
         return render(request, "accounts/login.html")
 
