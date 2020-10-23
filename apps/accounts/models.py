a0093ea1364662389abadbaba68e9e7a7288a692
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2020-10-23 13:40:18 -0700

    super

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -21,11 +21,13 @@ class MyAccountManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, username, password):
+    def create_superuser(self, first_name, last_name, email, username, password):
         user = self.create_user(
             email=self.normalize_email(email),
             password=password,
             username=username,
+            first_name=first_name,
+            last_name=last_name
         )
         user.is_admin = True
         user.is_staff = True
