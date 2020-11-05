fd9f3c3da0c7d1182cddc694b3f81286c7ade192
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2020-11-05 10:15:53 -0800

    add first and last name fields to update user form

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,7 +7,7 @@ class UserUpdateForm(forms.ModelForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email']
+        fields = ['username', 'email', 'first_name', 'last_name']
 
 
 class ProfileUpdateForm(forms.ModelForm):
