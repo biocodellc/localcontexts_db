c19ae2153b873f7742f0afe774ffd3510eca33aa
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2020-11-09 12:58:50 -0800

    registration refactored into forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,18 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
+# from django.core.exceptions import ValidationError
+
+class RegistrationForm(UserCreationForm):
+    first_name = forms.CharField(label='First name', max_length=150)
+    last_name = forms.CharField(label='Last name', max_length=150)
+    email = forms.EmailField(max_length=200, help_text='Required')
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
