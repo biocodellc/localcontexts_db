8b2e90d0cabd5955de41e8104abd348d2b428f02
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2020-11-12 12:32:06 -0800

    add create-community template

--- communities/urls.py
+++ communities/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('connect-community', views.connect_community, name="connect-community"),
+    path('create-community', views.create_community, name="create-community"),
+    path('community-registry', views.community_registry, name="community-registry"),
 ]
