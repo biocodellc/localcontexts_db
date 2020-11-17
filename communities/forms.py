a6b490a2e865861979341669dadc4034dd8d445e
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2020-11-17 11:54:59 -0800

    dynamically populate community registry template with db data

---
+++ communities/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Community
+
+class CreateCommunityForm(forms.ModelForm):
+
+    class Meta:
+        model = Community
+        fields = ['community_name', 'country']
--- communities/views.py
+++ communities/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+# from .forms import CreateCommunityForm
+from .models import Community
 
 @login_required
 def connect_community(request):
@@ -9,7 +11,13 @@ def connect_community(request):
 def create_community(request):
     return render(request, 'communities/create-community.html')
 
+
 @login_required
 def community_registry(request):
-    return render(request, 'communities/community-registry.html')
+    communities = Community.objects.all()
+
+    context = {
+        'communities': communities
+    }
+    return render(request, 'communities/community-registry.html', context)
 
