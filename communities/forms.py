f46eb9e6839ab8dbc9127dae68a62c031404d799
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2020-11-17 14:19:09 -0800

    connected create-community form to the db

--- communities/forms.py
+++ communities/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from .models import Community
 
 class CreateCommunityForm(forms.ModelForm):
-
     class Meta:
         model = Community
         fields = ['community_name', 'country']
--- communities/views.py
+++ communities/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-# from .forms import CreateCommunityForm
+from .forms import CreateCommunityForm
 from .models import Community
 
 @login_required
@@ -9,7 +9,16 @@ def connect_community(request):
 
 @login_required
 def create_community(request):
-    return render(request, 'communities/create-community.html')
+
+    form = CreateCommunityForm()
+
+    if request.method == "POST":
+        form = CreateCommunityForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form': form}
+    return render(request, 'communities/create-community.html', context)
 
 
 @login_required
