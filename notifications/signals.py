6698c94d9172e6f9ceed9115dc4cb359b8f3c533
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2020-12-16 15:57:35 -0800

    adding a user to community sends a notification using a signal

--- notifications/signals.py
+++ notifications/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
+from communities.models import InviteMember
 from .models import *
 
 # Sends welcome notification to user that just registered
@@ -10,3 +11,34 @@ def create_welcome_message(sender, instance, created, **kwargs):
         UserNotification.objects.create(user=instance, title="Welcome", message="You have joined")
 
 post_save.connect(create_welcome_message, sender=User)
+
+# When the instance of invite member form is saved, send target user a notification
+@receiver(post_save, sender=InviteMember)
+def send_community_invite(sender, instance, created, **kwargs):
+    sender_ = instance.sender
+    receiver_ = instance.receiver
+
+    UserNotification.objects.create(user=receiver_, title="You've been invited to join a community!", message="Join our community.")
+
+
+#TODO: reference
+
+# @receiver(post_save, sender=CommunityInvitation)
+# def add_community_member(sender, created, instance, **kwargs):
+#     sender_ = instance.sender
+#     receiver_ = instance.receiver
+#     if instance.status=='accepted':
+        # If invitation is accepted
+        # Receiver's usercommunity is updated with sender's Community
+        # Community is updated with member based on role
+        # target_user=UserCommunity.objects.get(username=receiver_.user.username)
+        # target_user.communities.add(sender_.community_name)
+
+        # if instance.role=='editor':
+        #     sender_.editor.add(receiver_.user)
+
+        # elif instance.role=='viewer':
+        #     sender_.viewer.add(receiver_.user)
+
+        # sender_.save()
+        # target_user.save()
