2c0aaaa9e36e7ba38bbca32725f8367a31efa4c3
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2020-12-29 18:36:43 -0800

    fix user profile update page

--- accounts/forms.py
+++ accounts/forms.py
@@ -45,4 +45,4 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['profile_pic', 'job_title', 'city_or_town', 'country', 'registration_reason']
+        fields = ['profile_pic', 'job_title', 'city_or_town', 'country']
