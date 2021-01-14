53f29be4833cd44feb3c62b8d3383d3dd3fb9708
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-01-14 14:31:08 -0800

    institutions: add basic templates, views, configs

--- institutions/urls.py
+++ institutions/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('connect-institution/', views.connect_institution, name="connect-institution"),
+    path('create-institution/', views.create_institution, name="create-institution"),
+    path('institution-registry/', views.institution_registry, name="institution-registry"),
 ]
