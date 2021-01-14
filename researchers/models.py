d294a44c5184def69d87050b4d277d761d48123f
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-01-14 11:14:53 -0800

    update project and researcher models

--- researchers/models.py
+++ researchers/models.py
@@ -3,12 +3,16 @@ from django.contrib.auth.models import User
 
 # Note: Right now all these fields are not required.
 class Project(models.Model):
-    who = models.TextField(blank=True, null=True)
-    when = models.TextField(blank=True, null=True)
-    where = models.TextField(blank=True, null=True)
-    what = models.TextField(blank=True, null=True)
+    title = models.TextField(blank=True, null=True)
+    contributor = models.TextField(blank=True, null=True)
+    location = models.TextField(blank=True, null=True)
     abstract = models.TextField(blank=True, null=True)
     target_species = models.TextField(blank=True, null=True)
+    source = models.TextField(blank=True, null=True)
+    url = models.TextField(blank=True, null=True)
+    publication_date = models.TextField(blank=True, null=True)
+    date_added = models.TextField(blank=True, null=True)
+    date_modified = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.who
@@ -17,7 +21,7 @@ class Researcher(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     # Note: for now orcid is not required
     orcid = models.CharField(max_length=16, blank=True, null=True)
-    project = models.ManyToManyField(Project, blank=True)
+    projects = models.ManyToManyField(Project, blank=True)
 
     def __str__(self):
         return str(self.user)
