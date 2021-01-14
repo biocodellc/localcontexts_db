0f9d281b425654fa2b8997147bef3ca025b236b0
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-01-14 13:53:40 -0800

    adjust bullet points on registration template

--- institutions/urls.py
+++ institutions/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('connect-institution', views.connect_institution, name="connect-institution"),
+    path('connect-institution/', views.connect_institution, name="connect-institution"),
 ]
--- researchers/urls.py
+++ researchers/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('connect-researcher', views.connect_researcher, name="connect-researcher"),
+    path('connect-researcher/', views.connect_researcher, name="connect-researcher"),
 ]
