1060d703741256a294c0f690309a9484db596851
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-01-14 11:00:38 -0800

    update institution models

--- institutions/models.py
+++ institutions/models.py
@@ -4,14 +4,17 @@ from django_countries.fields import CountryField
 
 
 class Institution(models.Model):
+    institution_creator = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     image = models.ImageField(upload_to='photos/institutions', blank=True, null=True)
     institution_name = models.CharField(max_length=80, blank=True, null=True)
     institution_code = models.CharField(max_length=80, blank=True, null=True)
     address = models.CharField(max_length=80, blank=True, null=True)
     contact_name = models.CharField(max_length=80, blank=True, null=True)
     contact_email = models.EmailField(max_length=254, blank=True, null=True)
+    town = models.CharField(max_length=80, blank=True, null=True)
     country = CountryField(blank=True, null=True)
-    members = models.ManyToManyField(User, blank=True)
+    members = models.ManyToManyField(User, blank=True, related_name="institution_members")
+    is_approved = models.BooleanField(default=False, null=True)
 
     def __str__(self):
         return self.institution_name
@@ -22,7 +25,7 @@ class Institution(models.Model):
 
 class UserInstitution(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
-    institution = models.ManyToManyField(Institution, blank=True)
+    institutions = models.ManyToManyField(Institution, blank=True)
 
     def __str__(self):
         return str(self.user)
