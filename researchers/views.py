f26b63960e36f9241af81ab8d31fdbd5cdf409c7
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-01-27 14:31:48 -0800

    add notice message on project creation

--- researchers/views.py
+++ researchers/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.decorators import login_required
 from bclabels.models import BCNotice
 
 from .models import Researcher
-from .forms import ConnectResearcherForm, CreateProjectForm, ProjectContributorsForm
+from .forms import *
 
 @login_required(login_url='login')
 def connect_researcher(request):
@@ -42,14 +42,12 @@ def researcher_notices(request, pk):
 @login_required(login_url='login')
 def add_notice(request, pk):
     researcher = Researcher.objects.get(id=pk)
-    proj_form = CreateProjectForm()
-    contrib_form = ProjectContributorsForm()
 
     if request.method == 'POST':
         proj_form = CreateProjectForm(request.POST)
         contrib_form = ProjectContributorsForm(request.POST)
 
-        if proj_form.is_valid() and contrib_form.is_valid():
+        if proj_form.is_valid() and contrib_form.is_valid():            
             proj = proj_form.save(commit=False)
             contrib = contrib_form.save(commit=False)
             contrib.project = proj
@@ -60,12 +58,14 @@ def add_notice(request, pk):
             # Saves project to researcher's list of projects
             researcher = Researcher.objects.get(user=request.user)
             researcher.projects.add(proj)
+
+            message = request.POST.get('contrib-message') # Get value of message
             
-            created_notice = BCNotice.objects.create(project=proj, placed_by_researcher=researcher)
+            created_notice = BCNotice.objects.create(project=proj, placed_by_researcher=researcher, message=message)
 
             if contrib.community:
                 created_notice.communities.add(contrib.community)
-    
+                
             context = {
                 'researcher': researcher,
                 'proj_form': proj_form,
@@ -73,15 +73,17 @@ def add_notice(request, pk):
             }
 
             return render(request, 'researchers/add-notice.html', context)
-    
+    else:
+        proj_form = CreateProjectForm()
+        contrib_form = ProjectContributorsForm()
         
-    context = {
-        'researcher': researcher,
-        'proj_form': proj_form,
-        'contrib_form': contrib_form,
-    }
+        context = {
+            'researcher': researcher,
+            'proj_form': proj_form,
+            'contrib_form': contrib_form,
+        }
 
-    return render(request, 'researchers/add-notice.html', context)
+        return render(request, 'researchers/add-notice.html', context)
 
 @login_required(login_url='login')
 def researcher_relationships(request, pk):
