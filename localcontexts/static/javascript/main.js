191e8e7bf4c898febcd9330fbd0a6e0cf1943d56
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-01-28 14:30:20 -0800

    clicking on labels sets default text and image of labels

--- localcontexts/static/javascript/main.js
+++ localcontexts/static/javascript/main.js
@@ -73,7 +73,7 @@ function showMore(elem) {
 }
 
 function showMoreTemporary() {
-    let div = document.getElementById(`proj-expand`)
+    let div = document.getElementById('proj-expand')
     let span = document.getElementById('plus-minus')
 
     if (div.style.height == "0px" && span.textContent == "+") {
@@ -85,4 +85,54 @@ function showMoreTemporary() {
         span.textContent = "+"
     }
 }
+
+// Default label text
+function setDefaultText(img) {
+    let defaultText = document.getElementById('bc-label-default-txt')
+    let defaultImg = document.getElementById('chosen-label-img')
+    let targetImg = img.id
+    let source = defaultImg.src
+
+    let matches = source.match(/(?<=bc-labels\/bc-).*/)
+    let targetStr = matches[0]
+    console.log(source)
+    console.log(targetStr)
+
+
+    const provenanceText = 'This Label is being used to affirm an inherent interest Indigenous people have in the scientific collections and data about communities, peoples, and the biodiversity found within traditional lands, waters and territories. [Community name or authorizing party] has permissioned the use of this collection and associated data for research purposes, and retains the right to be named and associated with it into the future. This association reflects a significant relationship and responsibility to [the species or biological entity] and associated scientific collections and data.'
+    const multipleCommunityText = 'This Label is being used to affirm responsibility and ownership over this information, collection, data and digital sequence information is spread across several distinct communities. Use will be dependent upon discussion and negotiation with multiple communities.'
+    const openToCollabText = 'This Label is being used to make clear [community name or authorizing body] is open to future engagement, collaboration, and partnership around research and outreach opportunities.'
+    const openToCommercializationText = 'This Label is being used to indicate that [community name or authorizing party] is open to commercialization opportunities that might derive from any information, collections, data and DSI to which this Label is connected. As a primary party in any partnership and collaboration opportunities that emerge from the use of these resources, we retain an express interest in any future negotiations.'
+    const researchUseText = 'This Label is being used by [community name or authorizing body] to allow this information, collection, data and digital sequence information to be used for unspecified research purposes. This Label does not provide permission for commercialization activities.  [Optional return of research results statement].'
+    const consentVerified = 'This Label is being used to verify that [community name or authorizing party] have consent conditions in place for the use of this information, collections, data and digital sequence information.'
+
+    switch (targetImg) {
+        case 'bc-research-use-img':
+            defaultText.textContent = researchUseText
+            defaultImg.src = source.replace(targetStr, 'research-use.png')
+            break;
+        case 'bc-consent-verified':
+            defaultText.textContent = consentVerified
+            defaultImg.src = source.replace(targetStr, 'consent-verified.png')
+            break;
+        case 'bc-open-to-commercialization-img':
+            defaultText.textContent = openToCommercializationText
+            defaultImg.src = source.replace(targetStr, 'open-to-commercialization.png')
+            break;
+        case 'bc-open-to-collaboration-img':
+            defaultText.textContent = openToCollabText
+            defaultImg.src = source.replace(targetStr, 'open-to-collaboration.png')
+            break;
+        case 'bc-multiple-community-img':
+            defaultText.textContent = multipleCommunityText
+            defaultImg.src = source.replace(targetStr, 'multiple-community.png')
+            break;
+        case 'bc-provenance-label-img':
+            defaultText.textContent = provenanceText
+            defaultImg.src = source.replace(targetStr, 'provenance.png')
+            break;
+    }
+}
+
+
 
