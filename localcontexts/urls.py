1c866fbc6d34a6d83d87dc7f471e70c951008360
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-02-02 15:28:15 -0800

    remove static pages

--- localcontexts/urls.py
+++ localcontexts/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('pages.urls')),
+    # path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('communities/', include('communities.urls')),
