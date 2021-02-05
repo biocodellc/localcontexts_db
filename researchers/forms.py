a216cb37f65810143091dacf437e3f9eaaa1e3ec
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-02-05 15:23:01 -0800

    update add notice form fields

--- researchers/forms.py
+++ researchers/forms.py
@@ -14,13 +14,23 @@ class ConnectResearcherForm(forms.ModelForm):
 class CreateProjectForm(forms.ModelForm):
     class Meta:
         model = Project
-        fields = ['title', 'is_public', 'description', 'source', 'url', 'publication_date', 'principal_investigator', 'principal_investigator_affiliation', 'project_contact', 'project_contact_email', 'publication_doi', 'project_data_guid', 'recommended_citation']
+        fields = ['title', 'is_public', 'description', 'source', 'url', 'publication_date', 'principal_investigator', 'principal_investigator_affiliation', 'project_contact', 'project_contact_email', 'publication_doi', 'project_data_guid', 'recommended_citation', 'geome_project_id']
         widgets = {
             'title': forms.Textarea(attrs={'rows': 1, 'cols':65}),
             'description': forms.Textarea(attrs={'rows': 4, 'cols':65}),
             'source': forms.Textarea(attrs={'rows': 1, 'cols':65}),
             'publication_date': forms.Textarea(attrs={'rows': 1, 'cols':65}),
             'is_public': forms.CheckboxInput(attrs={'class':'is-public-checkbox', 'style': 'width:20px;height:20px;'}),
+            'principal_investigator': forms.TextInput(attrs={'size': 65}),
+            'principal_investigator_affiliation': forms.TextInput(attrs={'size': 65}),
+            'project_contact': forms.TextInput(attrs={'size': 65}),
+            'project_contact_email': forms.TextInput(attrs={'size': 65}),
+            'publication_doi': forms.TextInput(attrs={'size': 65}),
+            'project_data_guid': forms.TextInput(attrs={'size': 65}),
+            'recommended_citation': forms.TextInput(attrs={'size': 65}),
+            'geome_project_id': forms.TextInput(attrs={'size': 65}),
+            'url': forms.TextInput(attrs={'size': 65}),
+
         }
 
 class ProjectContributorsForm(forms.ModelForm):
