d5c433fbd9e6be1e4c3714f4e7b0748407fcd8ed
Yurii Serhiichuk <savik.ne@gmail.com>
2021-02-06 22:12:40 +0200

    Explicitly check for `True` in the `DEBUG_VALUE`.

--- localcontexts/settings.py
+++ localcontexts/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['PROD_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG_VALUE']
+DEBUG = os.environ['DEBUG_VALUE'] == 'True'
 
 ALLOWED_HOSTS = [
     'localhost',
