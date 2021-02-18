e44fbd603f6bad52eb98722e46a9b38ae195ab45
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-02-18 13:00:26 -0800

    add dynamic template text and why use this label text in community:labels template

--- localcontexts/static/javascript/main.js
+++ localcontexts/static/javascript/main.js
@@ -86,6 +86,28 @@ function showMoreTemporary() {
     }
 }
 
+var provenanceName = '(BC P) Provenance | Ahunga Taketake'
+var multipleCommunityName = '(BC MC) Multiple Community BC Label | Hāpori Maha'
+var openToCollabName = '(BC OC) Open to Collaboration BC Label | Ka Rata Pea Kia Mahitahi'
+var openToCommercializationName = '(BC C) Open to Commercialization BC Label | Ka Rata Pea Kia Whakapakahi'
+var researchUseName = '(BC R) Research Use BC Label | Hei Rangahau'
+var consentVerifiedName = '(BC CV) Consent Verified BC Label | Whakaaetanga Manatoko'
+
+var provenanceUse = "Indigenous peoples have the right to make decisions about the future use of information, biological collections, data and digital sequence information (DSI) that derives from associated lands, waters and territories. This Label supports the practice of proper and appropriate acknowledgement into the future."
+var multipleCommUse = "This Label should be used to indicate that multiple communities have responsibility, custodianship and/or ownership over the geographic regions where this species or biological entity originates/is found. This Label recognizes that whilst one community might exert specific authority, other communities also have rights and responsibilities for use and care."
+var openToCollabUse = "This Label is being used to indicate that the community is open to research collaborations and outreach activities. With this Label, future opportunities for collaboration and engagement are supported."
+var openToCommUse = "Indigenous peoples have the right to benefit from information, biological collections, data and digital sequence information (DSI) that derives from traditional lands, waters and territories. This Label is being used to indicate the express interest that [community name or authorizing party] has in being a primary party to any future negotiations if future commercialization opportunities arise from these resources."
+var researchUse = " This label should be used for permissioning the use of information, collections, data and digital sequence information for unspecified research. The research use label does not give permission for commercialization activities."
+var consentVerifiedUse = "Indigenous peoples have the right to permission the use of information, biological collections, data and digital sequence information (DSI) that derives from associated lands, waters and territories. This Label verifies that there are consent conditions in place for uses of information, collections, data and digital sequence information."
+
+var provenanceText = 'This Label is being used to affirm an inherent interest Indigenous people have in the scientific collections and data about communities, peoples, and the biodiversity found within traditional lands, waters and territories. [Community name or authorizing party] has permissioned the use of this collection and associated data for research purposes, and retains the right to be named and associated with it into the future. This association reflects a significant relationship and responsibility to [the species or biological entity] and associated scientific collections and data.'
+var multipleCommunityText = 'This Label is being used to affirm responsibility and ownership over this information, collection, data and digital sequence information is spread across several distinct communities. Use will be dependent upon discussion and negotiation with multiple communities.'
+var openToCollabText = 'This Label is being used to make clear [community name or authorizing body] is open to future engagement, collaboration, and partnership around research and outreach opportunities.'
+var openToCommercializationText = 'This Label is being used to indicate that [community name or authorizing party] is open to commercialization opportunities that might derive from any information, collections, data and DSI to which this Label is connected. As a primary party in any partnership and collaboration opportunities that emerge from the use of these resources, we retain an express interest in any future negotiations.'
+var researchUseText = 'This Label is being used by [community name or authorizing body] to allow this information, collection, data and digital sequence information to be used for unspecified research purposes. This Label does not provide permission for commercialization activities.  [Optional return of research results statement].'
+var consentVerifiedText = 'This Label is being used to verify that [community name or authorizing party] have consent conditions in place for the use of this information, collections, data and digital sequence information.'
+
+
 // Default label text -- Attach Labels
 function setDefaultText(img) {
     let defaultText = document.getElementById('id_default_text')
@@ -100,20 +122,6 @@ function setDefaultText(img) {
     // Find everything in the url after 'bc-labels/bc-' and get the match
     let matches = source.match(/(?<=bc-labels\/bc-).*/)
     let targetStr = matches[0]
-
-    const provenanceName = '(BC P) Provenance | Ahunga Taketake'
-    const multipleCommunityName = '(BC MC) Multiple Community BC Label | Hāpori Maha'
-    const openToCollabName = '(BC OC) Open to Collaboration BC Label | Ka Rata Pea Kia Mahitahi'
-    const openToCommercializationName = '(BC C) Open to Commercialization BC Label | Ka Rata Pea Kia Whakapakahi'
-    const researchUseName = '(BC R) Research Use BC Label | Hei Rangahau'
-    const consentVerifiedName = '(BC CV) Consent Verified BC Label | Whakaaetanga Manatoko'
-
-    const provenanceText = 'This Label is being used to affirm an inherent interest Indigenous people have in the scientific collections and data about communities, peoples, and the biodiversity found within traditional lands, waters and territories. [Community name or authorizing party] has permissioned the use of this collection and associated data for research purposes, and retains the right to be named and associated with it into the future. This association reflects a significant relationship and responsibility to [the species or biological entity] and associated scientific collections and data.'
-    const multipleCommunityText = 'This Label is being used to affirm responsibility and ownership over this information, collection, data and digital sequence information is spread across several distinct communities. Use will be dependent upon discussion and negotiation with multiple communities.'
-    const openToCollabText = 'This Label is being used to make clear [community name or authorizing body] is open to future engagement, collaboration, and partnership around research and outreach opportunities.'
-    const openToCommercializationText = 'This Label is being used to indicate that [community name or authorizing party] is open to commercialization opportunities that might derive from any information, collections, data and DSI to which this Label is connected. As a primary party in any partnership and collaboration opportunities that emerge from the use of these resources, we retain an express interest in any future negotiations.'
-    const researchUseText = 'This Label is being used by [community name or authorizing body] to allow this information, collection, data and digital sequence information to be used for unspecified research purposes. This Label does not provide permission for commercialization activities.  [Optional return of research results statement].'
-    const consentVerifiedText = 'This Label is being used to verify that [community name or authorizing party] have consent conditions in place for the use of this information, collections, data and digital sequence information.'
 
     switch (targetImg) {
         case 'bc-research-use-img':
@@ -185,12 +193,9 @@ function showBCLabelInfo() {
 
 // See more info about each label
 function expandBCLabel(img) {
-    // let targetImg = img.id
-    // console.log(targetImg)
     let div = document.getElementById('bclabel-info')
     let card = document.getElementById('collapsed-card')
     let imgContainer = document.getElementById('expand-bclabels')
-
 
     if (div.style.height == "0px") {
         imgContainer.style.height = "720px"
@@ -200,6 +205,37 @@ function expandBCLabel(img) {
         imgContainer.style.height = "460px"
         div.style.height = "0px"
         card.style.height = "460px"
+    }
+
+    let targetImg = img.id
+    let templateText = document.getElementById('label-template-text')
+    let whyUseLabelText = document.getElementById('why-use-this-label')
+
+    switch (targetImg) {
+        case 'bcr':
+            templateText.textContent = researchUseText
+            whyUseLabelText.textContent = researchUse
+            break;
+        case 'bccv':
+            templateText.textContent = consentVerifiedText
+            whyUseLabelText.textContent = consentVerifiedUse
+            break;
+        case 'bcocomm':
+            templateText.textContent = openToCommercializationText
+            whyUseLabelText.textContent = openToCommUse
+            break;
+        case 'bcocoll':
+            templateText.textContent = openToCollabText
+            whyUseLabelText.textContent = openToCollabUse
+            break;
+        case 'bcmc':
+            templateText.textContent = multipleCommunityText
+            whyUseLabelText.textContent = multipleCommUse
+            break;
+        case 'bcp':
+            templateText.textContent = provenanceText
+            whyUseLabelText.textContent = provenanceUse
+            break;
     }
 
 }
