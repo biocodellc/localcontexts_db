b412938f83105e2c839c42375927515d680723dc
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-03-19 12:41:12 -0700

    update accounts admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import Profile, UserAffiliation, SignUpInvitation
 
+class SignUpInvitationAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'email', 'date_sent')
+
 admin.site.register(Profile)
 admin.site.register(UserAffiliation)
-admin.site.register(SignUpInvitation)
+admin.site.register(SignUpInvitation, SignUpInvitationAdmin)
 
 
