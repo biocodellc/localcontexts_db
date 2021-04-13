db01cccebab99989a6a5cbe52046998eea86f406
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-04-13 14:52:56 -0700

    start ror integration

--- localcontexts/static/javascript/main.js
+++ localcontexts/static/javascript/main.js
@@ -754,3 +754,45 @@ function selectCommunities() {
 
     })
 }
+
+// TODO: Add ROR functionality
+// Create Institution
+// const endpoint = `http://api.ror.org/organizations`
+// const institutions = []
+// fetch(endpoint)
+//     .then(res => res.json())
+//     .then(data => institutions.push(...data.items))
+
+// function findMatches(wordToMatch, institutions) {
+//     return institutions.filter(org => {
+//         const regex = new RegExp(wordToMatch, 'gi')
+//         return org.name.match(regex)
+//     })
+// }
+
+// function displayMatches() {
+//     const matchArray = findMatches(this.value, institutions)
+//     const html = matchArray.map(org => {
+//         const regex = new RegExp(this.value, 'gi')
+//         const orgName = org.name.replace(regex, `<span class="hl">${this.value}</span>`)
+//         return `
+//             <li onmouseover="getOrgName(this)">
+//                 <span class="name">${orgName}</span>
+//             </li>
+//         `
+//     }).join('')
+//     suggestions.innerHTML = html
+// }
+
+// const searchInput = document.querySelector('.search')
+// const suggestions = document.querySelector('.suggestions')
+
+// searchInput.addEventListener('change', displayMatches);
+// searchInput.addEventListener('keyup', displayMatches);
+
+// function getOrgName(elem) {
+//     // console.log(elem.innerText)
+//     searchInput.value = elem.innerText
+//     let ul = document.getElementById('institution-suggestions')
+//     // console.log(searchInput)
+// }
