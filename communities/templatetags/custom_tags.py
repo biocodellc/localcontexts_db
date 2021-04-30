833e4b2454ed7081891b0a9e64ed3cae50771ca7
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-04-30 15:51:47 -0700

    add anchor tag take user from notifications/requests to specific notice

--- communities/templatetags/custom_tags.py
+++ communities/templatetags/custom_tags.py
@@ -11,3 +11,7 @@ def get_label_count(community):
     tklabels = TKLabel.objects.filter(community=community).count()
     total_labels = bclabels + tklabels
     return total_labels
+
+@register.simple_tag
+def anchor(url_name, section_id, community_id):
+    return reverse(url_name, kwargs={'pk': community_id}) + "#full-notice-card-" + str(section_id)
