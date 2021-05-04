e0e7b1f1bdccbd5c1398bf56eec8fc634bac2846
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-05-04 16:47:11 -0700

    create custom template tags for institution total notices and projects, take out utils and unused functions, clean up views

--- institutions/templatetags/custom_institution_tags.py
+++ institutions/templatetags/custom_institution_tags.py
@@ -1,6 +1,9 @@
 from django import template
 from django.urls import reverse
 from notifications.models import InstitutionNotification
+from bclabels.models import BCNotice
+from tklabels.models import TKNotice
+from projects.models import ProjectContributors
 
 register = template.Library()
 
@@ -12,3 +15,15 @@ def institution_notifications(institution):
 @register.simple_tag
 def anchor(url_name, section_id, institution_id):
     return reverse(url_name, kwargs={'pk': institution_id}) + "#full-notice-card-" + str(section_id)
+
+@register.simple_tag
+def get_notices_count(institution):
+    bcnotices = BCNotice.objects.filter(placed_by_institution=institution).count()
+    tknotices = TKNotice.objects.filter(placed_by_institution=institution).count()
+    total = bcnotices + tknotices
+    return total
+
+@register.simple_tag
+def get_projects_count(institution):
+    contrib_count = ProjectContributors.objects.filter(institution=institution).count()
+    return contrib_count
