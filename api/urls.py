c0b7478507dc30dd372dac1995218e35e3c4824b
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-05-18 16:29:13 -0700

    add api routes to bclabel, tklabel, project and detail for each with only GET requests

--- api/urls.py
+++ api/urls.py
@@ -4,9 +4,22 @@ from . import views
 from rest_framework import routers
 from .api import *
 
-router = routers.DefaultRouter()
-router.register('v1/bclabels', BCLabelViewSet, 'bclabels')
-router.register('v1/tklabels', TKLabelViewSet, 'tklabels')
-router.register('v1/projects', ProjectViewSet, 'projects')
+# router = routers.DefaultRouter()
+# router.register('v1/bclabels', BCLabelViewSet, 'bclabels')
+# router.register('v1/tklabels', TKLabelViewSet, 'tklabels')
+# router.register('v1/projects', ProjectViewSet, 'projects')
 
-urlpatterns = router.urls
+# urlpatterns = router.urls
+
+urlpatterns = [
+    path('v1/', views.apiOverview, name="api-overview"),
+
+    path('v1/bclabels/', views.bclabels, name="api-bclabels"),
+    path('v1/bclabel/<str:unique_id>/', views.bclabel_detail, name="api-bclabel-detail"),
+
+    path('v1/tklabels/', views.tklabels, name="api-tklabels"),
+    path('v1/tklabel/<str:unique_id>/', views.tklabel_detail, name="api-tklabel-detail"),
+
+    path('v1/projects/', views.projects, name="api-projects"),
+    path('v1/project/<str:pk>/', views.project_detail, name="api-project-detail"),
+]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import JsonResponse
+from .serializers import *
+from bclabels.models import BCLabel
+from tklabels.models import TKLabel
+from projects.models import Project
 
-# Create your views here.
+@api_view(['GET'])
+def apiOverview(request):
+    # TODO: Project should be unique_id not pk
+    api_urls = {
+        'BCLabels': '/bclabels/',
+        'BCLabel Detail View': '/bclabel/<str:unique_id>',
+        'TKLabels': '/tklabels/',
+        'TKLabel Detail View': '/tklabel/<str:unique_id>',
+        'Projects': '/projects/',
+        'Project Detail View': '/project/<str:pk>',
+    }
+    return Response(api_urls)
+
+@api_view(['GET'])
+def bclabels(request):
+    bclabels = BCLabel.objects.all()
+    serializer = BCLabelSerializer(bclabels, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def bclabel_detail(request, unique_id):
+    bclabel = BCLabel.objects.get(unique_id=unique_id)
+    serializer = BCLabelSerializer(bclabel, many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def tklabels(request):
+    tklabels = TKLabel.objects.all()
+    serializer = TKLabelSerializer(tklabels, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def tklabel_detail(request, unique_id):
+    tklabel = TKLabel.objects.get(unique_id=unique_id)
+    serializer = TKLabelSerializer(tklabel, many=False)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def projects(request):
+    projects = Project.objects.all()
+    serializer = ProjectSerializer(projects, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def project_detail(request, pk):
+    # TODO: Project should be unique_id not pk
+    projects = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(projects, many=False)
+    return Response(serializer.data)
