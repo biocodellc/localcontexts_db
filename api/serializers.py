3bf76fa9a2b286b1dbccd4c65e5dc369bc7b7c41
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-05-18 15:49:15 -0700

    add tklabel and project api endpoints

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
-from bclabels.models import *
+from bclabels.models import BCLabel
+from tklabels.models import TKLabel
+from projects.models import Project
 
 class BCLabelSerializer(serializers.ModelSerializer):
     class Meta:
         model = BCLabel
+        fields = '__all__'
+
+class TKLabelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TKLabel
+        fields = '__all__'
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
         fields = '__all__'
--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 from rest_framework import routers
-from .api import BCLabelViewSet
+from .api import *
 
 router = routers.DefaultRouter()
 router.register('v1/bclabels', BCLabelViewSet, 'bclabels')
+router.register('v1/tklabels', TKLabelViewSet, 'tklabels')
+router.register('v1/projects', ProjectViewSet, 'projects')
 
 urlpatterns = router.urls
