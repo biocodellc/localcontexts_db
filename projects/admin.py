e89bf4175143dceadd3c71af88db2c48e035d90f
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-05-19 11:34:30 -0700

    add uuid field to project model and run migrations

--- projects/admin.py
+++ projects/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import Project, ProjectContributors
 
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('title', 'project_contact', 'project_contact_email', 'is_public', 'date_added')
+    list_display = ('title', 'project_contact', 'project_contact_email', 'is_public', 'created')
+    readonly_fields = ('unique_id', 'created')
 
 class ProjectContributorsAdmin(admin.ModelAdmin):
     list_display = ('project', 'institution', 'community', 'researcher')
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,4 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
 from institutions.models import Institution
@@ -12,6 +13,7 @@ class Project(models.Model):
         ('Publication', 'Publication'),
         ('Sample', 'Sample'),
     )
+    unique_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     project_creator = models.ForeignKey(User, null=True, on_delete=models.CASCADE, related_name="project_creator")
     project_type = models.CharField(max_length=20, null=True, choices=TYPES)
     title = models.CharField(max_length=300, null=True)
@@ -25,8 +27,8 @@ class Project(models.Model):
     geome_project_id = models.IntegerField(blank=True, null=True)
     url = models.URLField(blank=True, null=True)
     publication_date = models.DateField(null=True, blank=True)
-    date_added = models.DateTimeField(auto_now_add=True, null=True)
-    date_modified = models.DateTimeField(auto_now=True, null=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now=True, null=True)
     is_public = models.BooleanField(default=True, null=True)
     bclabels = models.ManyToManyField("bclabels.BCLabel", verbose_name="BC Labels", blank=True, related_name="project_labels")
     tklabels = models.ManyToManyField("tklabels.TKLabel", verbose_name="TK Labels", blank=True, related_name="project_tklabels")
@@ -43,7 +45,7 @@ class Project(models.Model):
         return self.title
     
     class Meta:
-        ordering = ('-date_added',)
+        ordering = ('-created',)
 
 class ProjectContributors(models.Model):
     project = models.OneToOneField(Project, null=True, on_delete=models.CASCADE)
