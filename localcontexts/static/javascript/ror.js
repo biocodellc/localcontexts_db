3f9a268dd194c40a49ccb3ae8d71cb8a8af44eef
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-05-27 14:59:55 -0700

    take out unused searchbar code

--- localcontexts/static/javascript/ror.js
+++ localcontexts/static/javascript/ror.js
@@ -13,8 +13,6 @@ let removeAllOptions = (selectBox) => {
 
 let getCountry = () => {
     let countryCode = countrySelect.value.toLowerCase()
-    // TODO: Catch error when institutions don't exist for a country / there is no JSON file. 
-    // Example: Anguilla and American Samoa has no json and no institutions
 
     // Based on what was selected in the dropdown, enter into endpoint and fetch
     const endpoint = `https://raw.githubusercontent.com/biocodellc/ror-parser/main/data/${countryCode}.json`
@@ -23,6 +21,8 @@ let getCountry = () => {
             if (res.ok) {
                 return res.json()
             } else if (res.status === 404) {
+                // Catch error when institutions don't exist for a country / there is no JSON file. 
+                // Example: Anguilla and American Samoa has no json / no institutions
                 return Promise.reject('404 Not Found')
             }
         })
@@ -82,39 +82,3 @@ let populateInstitutions = () => {
 
 countrySelect.addEventListener('change', getCountry)
 institutionTypeSelect.addEventListener('change', populateInstitutions)
-
-
-// Search by typing something in
-// function findMatches(wordToMatch, institutions) {
-//     return institutions.filter(org => {
-//         const regex = new RegExp(wordToMatch, 'gi')
-//         return org.name.match(regex)
-//     })
-// }
-
-// function displayMatches() {
-//     const matchArray = findMatches(this.value, institutions)
-//     const html = matchArray.map(org => {
-//         const regex = new RegExp(this.value, 'gi')
-//         const orgName = org.name.replace(regex, `<span class="hl">${this.value}</span>`)
-//         return `
-//             <li onmouseover="getOrgName(this)">
-//                 <span class="name">${orgName}</span>
-//             </li>
-//         `
-//     }).join('')
-//     suggestions.innerHTML = html
-// }
-
-// const searchInput = document.querySelector('.search')
-// const suggestions = document.querySelector('.suggestions')
-
-// searchInput.addEventListener('change', displayMatches);
-// searchInput.addEventListener('keyup', displayMatches);
-
-// function getOrgName(elem) {
-//     // console.log(elem.innerText)
-//     searchInput.value = elem.innerText
-//     let ul = document.getElementById('institution-suggestions')
-//     // console.log(searchInput)
-// }
