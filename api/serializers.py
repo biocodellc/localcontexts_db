301e77c9af7531164f8ac6d1fbb08a976dd0ad00
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-05-28 14:05:32 -0700

    add bcnotice api endpoints

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
-from bclabels.models import BCLabel
-from tklabels.models import TKLabel
+from bclabels.models import BCLabel, BCNotice
+from tklabels.models import TKLabel, TKNotice
 from projects.models import Project
 
 class BCLabelSerializer(serializers.ModelSerializer):
@@ -14,6 +14,23 @@ class TKLabelSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ProjectSerializer(serializers.ModelSerializer):
+    bclabels = BCLabelSerializer(many=True)
+    tklabels = TKLabelSerializer(many=True)
+
     class Meta:
         model = Project
+        fields = '__all__'
+
+class BCNoticeSerializer(serializers.ModelSerializer):
+    project = ProjectSerializer()
+
+    class Meta:
+        model = BCNotice
+        fields = '__all__'
+
+class TKNoticeSerializer(serializers.ModelSerializer):
+    project = ProjectSerializer()
+
+    class Meta:
+        model = TKNotice
         fields = '__all__'
