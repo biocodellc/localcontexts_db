33546a061d1f07049727062ae137aec8564773e6
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-06-25 16:23:08 -0700

    template updates

--- institutions/forms.py
+++ institutions/forms.py
@@ -5,16 +5,18 @@ from django.utils.translation import ugettext_lazy as _
 class CreateInstitutionForm(forms.ModelForm):
     class Meta:
         model = Institution
-        fields = ['institution_id', 'city_or_town', 'country', 'contact_name', 'contact_email']
+        fields = ['institution_id', 'city_or_town', 'state_or_province', 'country', 'contact_name', 'contact_email']
         widgets = {
             'contact_name': forms.TextInput(attrs={'class': 'w-100'}),
             'contact_email': forms.EmailInput(attrs={'class': 'w-100'}),
+            'city_or_town': forms.TextInput(attrs={'class': 'w-100'}),
+            'state_or_province': forms.TextInput(attrs={'class': 'w-100'}),
         }
 
 class CreateInstitutionNoRorForm(forms.ModelForm):
     class Meta:
         model = Institution
-        fields = ['institution_name','institution_id', 'city_or_town', 'country', 'contact_name', 'contact_email']
+        fields = ['institution_name','institution_id', 'city_or_town', 'state_or_province', 'country', 'contact_name', 'contact_email']
         error_messages = {
             'institution_name': {
                 'unique': _("An institution by that name already exists."),
@@ -24,6 +26,7 @@ class CreateInstitutionNoRorForm(forms.ModelForm):
             'contact_name': forms.TextInput(attrs={'class': 'w-100'}),
             'contact_email': forms.EmailInput(attrs={'class': 'w-100'}),
             'city_or_town': forms.TextInput(attrs={'class': 'w-100'}),
+            'state_or_province': forms.TextInput(attrs={'class': 'w-100'}),
         }
 
 class UpdateInstitutionForm(forms.ModelForm):
