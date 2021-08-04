f37c0ea3aa79643eb309a6a8119dc705d6b5ac43
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-08-04 17:45:18 +0300

    add list of notified communities to project overview

--- projects/templatetags/custom_project_tags.py
+++ projects/templatetags/custom_project_tags.py
@@ -1,9 +1,11 @@
 from django import template
 from institutions.models import Institution
 from researchers.models import Researcher
+from helpers.models import NoticeStatus
+from bclabels.models import BCNotice
+from tklabels.models import TKNotice
 
 register = template.Library()
-
 
 @register.simple_tag
 def get_all_researchers():
@@ -17,3 +19,18 @@ def get_all_institutions():
 def define(val=None):
     # To use: {% define 'oldVariable' as newVariable %}
   return val
+
+@register.simple_tag
+def which_communities_notified(project):
+    if project.project_bcnotice.all().exists():
+        bcnotice = BCNotice.objects.get(project=project)
+        statuses = NoticeStatus.objects.filter(bcnotice=bcnotice)
+        return statuses
+
+    elif project.project_tknotice.all().exists():
+        tknotice = TKNotice.objects.get(project=project)
+        statuses = NoticeStatus.objects.filter(tknotice=tknotice)
+        return statuses
+
+    # if yes, 
+    # check to see if notice status exists for a particular project then which community the status is associated with
