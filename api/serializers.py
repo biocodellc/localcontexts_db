767b893f55b2b2352831572431244686f7051695
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-08-09 14:04:30 +0300

    add notices to project API

--- api/serializers.py
+++ api/serializers.py
@@ -8,49 +8,72 @@ from institutions.models import Institution
 from researchers.models import Researcher
 from django.contrib.auth.models import User
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('first_name', 'last_name',)
-
 class CommunitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Community
         fields = ('community_name',)
 
-class InstitutionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Institution
-        fields = ('institution_name',)
-
 class ResearcherSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
+    user = SerializerMethodField()
 
     class Meta:
         model = Researcher
         fields = ('user', 'orcid')
 
+    def get_user(self, obj):
+        return str(obj.user.first_name) + ' ' + str(obj.user.last_name)
+
 class BCLabelSerializer(serializers.ModelSerializer):
-    community = CommunitySerializer()
+    community = SerializerMethodField()
 
     class Meta:
         model = BCLabel
         fields = ('name', 'label_type', 'default_text', 'img_url', 'community', 'created', 'updated')
+    
+    def get_community(self, obj):
+        return str(obj.community.community_name)
 
 class TKLabelSerializer(serializers.ModelSerializer):
-    community = CommunitySerializer()
+    community = SerializerMethodField()
 
     class Meta:
         model = TKLabel
         fields = ('name', 'label_type', 'default_text', 'img_url', 'community', 'created', 'updated')
 
+    def get_community(self, obj):
+        return str(obj.community.community_name)
+
+class BCNoticeSerializer(serializers.ModelSerializer):
+    placed_by_institution = SerializerMethodField()
+    placed_by_researcher = ResearcherSerializer()
+
+    class Meta:
+        model = BCNotice
+        fields = ('placed_by_researcher', 'placed_by_institution', 'unique_id', 'created', 'updated',)
+
+    def get_placed_by_institution(self, obj):
+        return str(obj.placed_by_institution)
+    
+class TKNoticeSerializer(serializers.ModelSerializer):
+    placed_by_institution = SerializerMethodField()
+    placed_by_researcher = ResearcherSerializer()
+
+    class Meta:
+        model = TKNotice
+        fields = ('placed_by_researcher', 'placed_by_institution', 'unique_id', 'created', 'updated',)
+    
+    def get_placed_by_institution(self, obj):
+        return str(obj.placed_by_institution)
+
 class ProjectSerializer(serializers.ModelSerializer):
     bclabels = BCLabelSerializer(many=True)
     tklabels = TKLabelSerializer(many=True)
+    bcnotice = BCNoticeSerializer(source="project_bcnotice", many=True)
+    tknotice = BCNoticeSerializer(source="project_tknotice", many=True)
 
     class Meta:
         model = Project
-        fields = ('unique_id', 'title', 'bclabels', 'tklabels')
+        fields = ('unique_id', 'title', 'bcnotice', 'tknotice', 'bclabels', 'tklabels')
 
     # def get_notices_or_labels(self, obj):
     #     if self.bclabels.all().exists() or self.tklabels.all().exists():
@@ -62,23 +85,3 @@ class ProjectSerializer(serializers.ModelSerializer):
     #             return self.project_tknotice
     #         else:
     #             return self
-
-class BCNoticeSerializer(serializers.ModelSerializer):
-    project = ProjectSerializer()
-    placed_by_institution = InstitutionSerializer()
-    placed_by_researcher = ResearcherSerializer()
-    communities = CommunitySerializer(many=True)
-
-    class Meta:
-        model = BCNotice
-        fields = ('project', 'communities', 'placed_by_researcher', 'placed_by_institution', 'unique_id', 'created', 'updated',)
-
-class TKNoticeSerializer(serializers.ModelSerializer):
-    project = ProjectSerializer()
-    placed_by_institution = InstitutionSerializer()
-    placed_by_researcher = ResearcherSerializer()
-    communities = CommunitySerializer(many=True)
-
-    class Meta:
-        model = TKNotice
-        fields = ('project', 'communities', 'placed_by_researcher', 'placed_by_institution', 'unique_id', 'created', 'updated',)
