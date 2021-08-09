de8a08bc0e121acdf6b6e69402d8f76e916c5426
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-08-09 22:10:38 +0300

    misc

--- api/views.py
+++ api/views.py
@@ -14,7 +14,7 @@ def apiOverview(request, format=None):
         'project detail view': '/projects/<str:unique_id>',
         'projects by username': '/projects/users/<str:username>',
         'projects by institution id': '/projects/institutions/<str:institution_id>',
-        'projects by researcher id': 'projects/researchers/<str:researcher_id>',
+        'projects by researcher id': '/projects/researchers/<str:researcher_id>',
     }
     return Response(api_urls)
 
