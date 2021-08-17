856b9f896e4100bed791c9c37c302b23568118ab
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-08-17 17:52:13 +0300

    add user deactivation to user settings

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('create-profile/', views.create_profile, name='create-profile'),
     path('update-profile/', views.update_profile, name='update-profile'),
+    path('deactivate/', views.deactivate_user, name='deactivate-user'),
 
     path('select-account/', views.registration_reason, name='select-account'),
     
