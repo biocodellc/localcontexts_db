0f8a28625d69f8a7ecc9313b1e535b23bb3345da
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-08-17 23:09:33 +0300

    add project level permissions to api

--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework import permissions
+from rest_framework.exceptions import PermissionDenied
 
 from .serializers import *
 from bclabels.models import BCLabel, BCNotice
@@ -20,34 +22,38 @@ def apiOverview(request, format=None):
 
 @api_view(['GET'])
 def projects(request):
-    projects = Project.objects.all()
+    projects = Project.objects.filter(is_discoverable=True, is_public=True)
     serializer = ProjectOverviewSerializer(projects, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
 def project_detail(request, unique_id):
     project = Project.objects.get(unique_id=unique_id)
-    serializer = ProjectSerializer(project, many=False)
-    return Response(serializer.data)
+    if project.is_public:
+        serializer = ProjectSerializer(project, many=False)
+        return Response(serializer.data)
+    else:
+        raise PermissionDenied({"message":"You don't have permission to view this project",
+                                "unique_id": unique_id})
 
 @api_view(['GET'])
 def projects_by_user(request, username):
     user = User.objects.get(username=username)
-    projects = Project.objects.filter(project_creator=user)
+    projects = Project.objects.filter(project_creator=user, is_discoverable=True, is_public=True)
     serializer = ProjectOverviewSerializer(projects, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
 def projects_by_institution(request, institution_id):
     institution = Institution.objects.get(id=institution_id)
-    projects = institution.projects.all()
+    projects = institution.projects.filter(is_discoverable=True, is_public=True)
     serializer = ProjectOverviewSerializer(projects, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
 def projects_by_researcher(request, researcher_id):
     researcher = Researcher.objects.get(id=researcher_id)
-    projects = researcher.projects.all()
+    projects = researcher.projects.filter(is_discoverable=True, is_public=True)
     serializers = ProjectOverviewSerializer(projects, many=True)
     return Response(serializers.data)
 
