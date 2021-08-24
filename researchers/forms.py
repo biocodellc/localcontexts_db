5a77a5127713f91012a29f139a855ed20dd38682
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-08-24 16:45:58 +0300

    add tooltips and adjust content

--- researchers/forms.py
+++ researchers/forms.py
@@ -4,9 +4,10 @@ from .models import Researcher
 class ConnectResearcherForm(forms.ModelForm):
     class Meta:
         model = Researcher
-        fields = ['primary_institution']
+        fields = ['primary_institution', 'description']
         widgets = {
             'primary_institution': forms.TextInput(attrs={'class': 'w-100'}),
+            'description': forms.Textarea(attrs={'class': 'w-100', 'rows': 3,}),
         }
 
 class UpdateResearcherForm(forms.ModelForm):
