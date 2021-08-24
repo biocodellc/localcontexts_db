ab4e7e3a6dced3ded81c13523acfe133ceb100a9
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-08-24 15:42:32 +0300

    change model fields in institution, community and profile from city_or_town and state_or_province to city_town and state_province_region

--- institutions/admin.py
+++ institutions/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Institution
 
 class InstitutionAdmin(admin.ModelAdmin):
-    list_display = ('institution_name', 'institution_creator', 'contact_name', 'contact_email', 'city_or_town', 'country')
+    list_display = ('institution_name', 'institution_creator', 'contact_name', 'contact_email', 'city_town', 'country')
 
 admin.site.register(Institution, InstitutionAdmin)
--- institutions/forms.py
+++ institutions/forms.py
@@ -5,18 +5,18 @@ from django.utils.translation import ugettext_lazy as _
 class CreateInstitutionForm(forms.ModelForm):
     class Meta:
         model = Institution
-        fields = ['institution_id', 'city_or_town', 'state_or_province', 'country', 'contact_name', 'contact_email']
+        fields = ['institution_id', 'city_town', 'state_province_region', 'country', 'contact_name', 'contact_email']
         widgets = {
             'contact_name': forms.TextInput(attrs={'class': 'w-100'}),
             'contact_email': forms.EmailInput(attrs={'class': 'w-100'}),
-            'city_or_town': forms.TextInput(attrs={'class': 'w-100'}),
-            'state_or_province': forms.TextInput(attrs={'class': 'w-100'}),
+            'city_town': forms.TextInput(attrs={'class': 'w-100'}),
+            'state_province_region': forms.TextInput(attrs={'class': 'w-100'}),
         }
 
 class CreateInstitutionNoRorForm(forms.ModelForm):
     class Meta:
         model = Institution
-        fields = ['institution_name','institution_id', 'city_or_town', 'state_or_province', 'country', 'contact_name', 'contact_email']
+        fields = ['institution_name','institution_id', 'city_town', 'state_province_region', 'country', 'contact_name', 'contact_email']
         error_messages = {
             'institution_name': {
                 'unique': _("An institution by that name already exists."),
@@ -25,17 +25,17 @@ class CreateInstitutionNoRorForm(forms.ModelForm):
         widgets = {
             'contact_name': forms.TextInput(attrs={'class': 'w-100'}),
             'contact_email': forms.EmailInput(attrs={'class': 'w-100'}),
-            'city_or_town': forms.TextInput(attrs={'class': 'w-100'}),
-            'state_or_province': forms.TextInput(attrs={'class': 'w-100'}),
+            'city_town': forms.TextInput(attrs={'class': 'w-100'}),
+            'state_province_region': forms.TextInput(attrs={'class': 'w-100'}),
         }
 
 class UpdateInstitutionForm(forms.ModelForm):
     class Meta:
         model = Institution
-        fields = ['contact_name', 'contact_email', 'city_or_town', 'state_or_province', 'country', 'image']
+        fields = ['contact_name', 'contact_email', 'city_town', 'state_province_region', 'country', 'image']
         widgets = {
             'contact_name': forms.TextInput(attrs={'class': 'w-100'}),
             'contact_email': forms.EmailInput(attrs={'class': 'w-100'}),
-            'state_or_province': forms.TextInput(attrs={'class': 'w-100'}),
-            'city_or_town': forms.TextInput(attrs={'class': 'w-100'}),
+            'state_province_region': forms.TextInput(attrs={'class': 'w-100'}),
+            'city_town': forms.TextInput(attrs={'class': 'w-100'}),
         }
