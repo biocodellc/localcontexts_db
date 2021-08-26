7bc509d509d82dec338a93e392306ede4d2218e9
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-08-26 21:23:40 +0300

    add orcid token to researcher model and assign at instance, fix researcher project error

--- researchers/models.py
+++ researchers/models.py
@@ -11,6 +11,7 @@ class Researcher(models.Model):
     website = models.URLField(max_length=150, blank=True, null=True)
     primary_institution = models.CharField(max_length=250, null=True, blank=True)
     projects = models.ManyToManyField('projects.Project', blank=True, related_name="researcher_projects")
+    orcid_auth_token = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return str(self.user)
