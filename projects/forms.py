be86641399dc91d1946c66aab6a6de7934999cdf
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-09-02 15:50:58 +0300

    take out project dates, update create project/edit project templates

--- projects/forms.py
+++ projects/forms.py
@@ -4,25 +4,30 @@ import datetime
 from .models import Project, ProjectContributors, ProjectPerson
 from django.utils.translation import ugettext_lazy as _
 
-class DateInput(forms.DateInput):
-    input_type = 'date'
-
 class CreateProjectForm(forms.ModelForm):
     PRIVACY = (
         ('Public', 'Public: Can be seen by anyone within and outside of the Local Contexts Hub'),
         ('Discoverable', 'Discoverable: Can be seen by authenticated contributors of the project only'),
         ('Private', 'Private: Can only be seen by project creator'),
     )
+    TYPES = (
+        ('Item', 'Item'),
+        ('Collection', 'Collection'),
+        ('Sample', 'Sample'),
+        ('Expedition', 'Expedition'),
+        ('Publication', 'Publication'),
+        ('Exhibition', 'Exhibition'),
+        ('Other', 'Other'),
+    )
     project_privacy = forms.ChoiceField(label=_('What is the privacy level of this project?'), choices=PRIVACY, widget=forms.RadioSelect())
+    project_type = forms.ChoiceField(label=_('What type of project are you creating?*'), choices=TYPES, widget=forms.Select(attrs={'class': 'w-100',}))
 
     class Meta:
         model = Project
-        fields = ['title', 'project_type', 'project_privacy', 'description', 'url', 'publication_date', 'publication_date_ongoing', 'project_contact', 'project_contact_email', 'publication_doi', 'project_data_guid', 'recommended_citation']
+        fields = ['title', 'project_type', 'project_privacy', 'description', 'url', 'project_contact', 'project_contact_email', 'publication_doi', 'project_data_guid', 'recommended_citation']
         widgets = {
-            'publication_date': DateInput(),
             'title': forms.TextInput(attrs={'class': 'w-100'}),
             'description': forms.Textarea(attrs={'rows': 4, 'class': 'w-100'}),
-            'publication_date_ongoing': forms.CheckboxInput(),
             'project_contact': forms.TextInput(attrs={'class': 'w-100'}),
             'project_contact_email': forms.TextInput(attrs={'class': 'w-100'}),
             'publication_doi': forms.TextInput(attrs={'class': 'w-100'}),
@@ -47,16 +52,24 @@ class EditProjectForm(forms.ModelForm):
         ('Discoverable', 'Discoverable: Can be seen by authenticated contributors of the project only'),
         ('Private', 'Private: Can only be seen by project creator'),
     )
-    project_privacy = forms.ChoiceField(label=_('What is the privacy level of this project?*'), choices=PRIVACY, widget=forms.RadioSelect())
+    TYPES = (
+        ('Item', 'Item'),
+        ('Collection', 'Collection'),
+        ('Sample', 'Sample'),
+        ('Expedition', 'Expedition'),
+        ('Publication', 'Publication'),
+        ('Exhibition', 'Exhibition'),
+        ('Other', 'Other'),
+    )
+    project_privacy = forms.ChoiceField(label=_('What is the privacy level of this project?'), choices=PRIVACY, widget=forms.RadioSelect())
+    project_type = forms.ChoiceField(label=_('What type of project are you creating?*'), choices=TYPES, widget=forms.Select(attrs={'class': 'w-100',}))
 
     class Meta:
         model = Project
-        fields = ['title', 'project_type', 'project_privacy', 'description', 'url', 'publication_date', 'publication_date_ongoing', 'project_contact', 'project_contact_email', 'publication_doi', 'project_data_guid', 'recommended_citation']
+        fields = ['title', 'project_type', 'project_privacy', 'description', 'url', 'project_contact', 'project_contact_email', 'publication_doi', 'project_data_guid', 'recommended_citation']
         widgets = {
-            'publication_date': DateInput(),
             'title': forms.TextInput(attrs={'class': 'w-100'}),
             'description': forms.Textarea(attrs={'rows': 4, 'class': 'w-100'}),
-            'publication_date_ongoing': forms.CheckboxInput(),
             'project_contact': forms.TextInput(attrs={'class': 'w-100'}),
             'project_contact_email': forms.TextInput(attrs={'class': 'w-100'}),
             'publication_doi': forms.TextInput(attrs={'class': 'w-100'}),
