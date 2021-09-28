0c1ed6e66e6a4e6c932eb58610290dc5dcf611a4
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-09-28 16:09:28 -0700

    add description to community settings

--- communities/forms.py
+++ communities/forms.py
@@ -30,13 +30,14 @@ class ValidateCommunityForm(forms.ModelForm):
 class UpdateCommunityForm(forms.ModelForm):
     class Meta:
         model = Community
-        fields = ['contact_name', 'contact_email', 'community_entity', 'city_town', 'state_province_region', 'country', 'image']
+        fields = ['contact_name', 'contact_email', 'description', 'community_entity', 'city_town', 'state_province_region', 'country', 'image']
         widgets = {
             'community_entity': forms.TextInput(attrs={'class': 'w-100'}),
             'contact_name': forms.TextInput(attrs={'class': 'w-100'}),
             'contact_email': forms.EmailInput(attrs={'class': 'w-100'}),
             'state_province_region': forms.TextInput(attrs={'class': 'w-100'}),
             'city_town': forms.TextInput(attrs={'class': 'w-100'}),
+            'description': forms.Textarea(attrs={'rows': 3, 'class': 'w-100'}),
         }
 
 class InviteMemberForm(forms.ModelForm):
