be81686a16043bed1129705e60adf20bb8b84c76
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-10-07 10:23:43 -0700

    rm comment

--- localcontexts/urls.py
+++ localcontexts/urls.py
@@ -19,7 +19,6 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 
 urlpatterns = [
-    # path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('communities/', include('communities.urls')),
