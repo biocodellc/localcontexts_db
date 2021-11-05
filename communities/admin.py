313dd9d1a298f255137b8cf0bf0905eeb6908c0d
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-11-05 15:41:54 -0700

    add date created to community and institution

--- communities/admin.py
+++ communities/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Community, InviteMember, JoinRequest
 
 class CommunityAdmin(admin.ModelAdmin):
-    list_display = ('community_name', 'community_creator', 'contact_name', 'contact_email', 'is_approved')
+    list_display = ('community_name', 'community_creator', 'contact_name', 'contact_email', 'is_approved', 'created')
     search_fields = ('community_name', 'contact_name', 'contact_email',)
 
 
--- institutions/admin.py
+++ institutions/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Institution
 
 class InstitutionAdmin(admin.ModelAdmin):
-    list_display = ('institution_name', 'institution_creator', 'contact_name', 'contact_email', 'is_approved')
+    list_display = ('institution_name', 'institution_creator', 'contact_name', 'contact_email', 'is_approved', 'created')
     search_fields = ('institution_name', 'contact_name', 'contact_email',)
 
 admin.site.register(Institution, InstitutionAdmin)
