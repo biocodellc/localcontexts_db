3b24de5d568fd35d152f0b4e35ae6f4896ce566e
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2021-12-14 14:27:30 -0800

    update project overview PDF

--- projects/views.py
+++ projects/views.py
@@ -3,9 +3,7 @@ from django.contrib.auth.decorators import login_required
 from .models import Project
 from helpers.utils import render_to_pdf, generate_zip
 from django.http import HttpResponse
-import urllib
 import requests
-from io import BytesIO
 
 def view_project(request, unique_id):
     project = Project.objects.get(unique_id=unique_id)
@@ -20,15 +18,19 @@ def download_project_zip(request, unique_id):
 
     # Create PDF from project context, append to files list
     pdf = render_to_pdf(template_path, context)
-    files.append((project.title + ".pdf", pdf))
+    # files.append((project.title + ".pdf", pdf))
+    files.append(("Project_Overview.pdf", pdf))
 
     # Usage guide PDF
     usage_guide_url = 'https://storage.googleapis.com/anth-ja77-local-contexts-8985.appspot.com/guides/LC-TK_BC-Notice-Usage-Guide_2021-11-16.pdf'
     response = requests.get(usage_guide_url) 
     files.append(('BC_TK_Label_Usage_Guide.pdf', response.content))
 
+    # TODO: 
+    # Label or Notice SVGs and .txt files
+    # README.txt
 
-
+    # Generate zip file 
     full_zip_in_memory = generate_zip(files)
 
     response = HttpResponse(full_zip_in_memory, content_type='application/force-download')
