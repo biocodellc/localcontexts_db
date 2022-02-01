6f5bcdd9e62f11caf11e1c07b55da29a59fac14c
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2022-02-01 11:50:58 -0800

    remove meatball menu, move download btn to view projects, rename view project and edit project btns

--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('project/<str:unique_id>/', views.view_project, name="view-project"),
-    path('project/download/<str:unique_id>/', views.download_project_zip, name="download-project-zip"),
+    path('<str:unique_id>/', views.view_project, name="view-project"),
+    path('download/<str:unique_id>/', views.download_project_zip, name="download-project-zip"),
 ]
