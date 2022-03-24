462def4c22d165e89a468905bb60f025163d0a76
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2022-03-24 14:18:08 -0700

    add default from email config

--- localcontexts/settings.py
+++ localcontexts/settings.py
@@ -195,6 +195,7 @@ EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 EMAIL_USE_TLS = True
 
 # Static files (CSS, JavaScript, Images)
