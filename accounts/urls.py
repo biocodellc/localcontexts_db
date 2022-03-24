e45b98d91ab214c1953a3f7b24fd9e8967ff81ed
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2022-03-24 14:03:46 -0700

    credential adjustments for emails

--- accounts/urls.py
+++ accounts/urls.py
@@ -30,17 +30,9 @@ urlpatterns = [
     path('counter/', views.hub_counter, name='hub-counter'),
 
 
-    path('reset-password/', 
-        auth_views.PasswordResetView.as_view(template_name="accounts/password-reset.html"), 
-        name="reset_password"),
-    path('reset-password-sent/', 
-        auth_views.PasswordResetDoneView.as_view(template_name="accounts/password-reset-sent.html"), 
-        name="password_reset_done"),
-    path('reset/<uidb64>/<token>/', 
-        auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password-reset-confirm.html"), 
-        name="password_reset_confirm"),
-    path('reset-password-complete/', 
-        auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password-reset-done.html"),    
-        name="password_reset_complete"),
+    path('reset-password/', auth_views.PasswordResetView.as_view(template_name="accounts/password-reset.html"), name="reset_password"),
+    path('reset-password-sent/', auth_views.PasswordResetDoneView.as_view(template_name="accounts/password-reset-sent.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password-reset-confirm.html"), name="password_reset_confirm"),
+    path('reset-password-complete/', auth_views.PasswordResetCompleteView.as_view(template_name="accounts/password-reset-done.html"), name="password_reset_complete"),
 
 ]
