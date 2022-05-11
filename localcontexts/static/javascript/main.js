6632f0f0fe3e9659be873afbed60cbc34bae461f
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2022-05-11 10:46:13 -0700

    prevent duplicate join requests by disabling the submit btn in registry

--- localcontexts/static/javascript/main.js
+++ localcontexts/static/javascript/main.js
@@ -1030,12 +1030,18 @@ if (window.location.href.includes('registry')) {
                 // show modal
                 registryModal.classList.replace('hide', 'show')
                 let targetId = e.target.id.split('-').pop()
-                submitJoinRequestFormBtn.addEventListener('click', function(e) { document.getElementById(`communityRegistryForm${targetId}`).submit() })    
+                submitJoinRequestFormBtn.addEventListener('click', function(e) { 
+                    disbleSendRequestBtn(submitJoinRequestFormBtn)
+                    document.getElementById(`communityRegistryForm${targetId}`).submit() 
+                })    
             } else if (e.target.id.includes('institutionRequest')) {
                 // show modal
                 registryModal.classList.replace('hide', 'show')
                 let targetId = e.target.id.split('-').pop()
-                submitJoinRequestFormBtn.addEventListener('click', function(e) { document.getElementById(`institutionRegistryForm${targetId}`).submit() })  
+                submitJoinRequestFormBtn.addEventListener('click', function(e) { 
+                    disbleSendRequestBtn(submitJoinRequestFormBtn)
+                    document.getElementById(`institutionRegistryForm${targetId}`).submit() 
+                })  
 
                 // open contact form modal
             } else if (e.target.id.includes('communityContact')) {
@@ -1058,6 +1064,20 @@ if (window.location.href.includes('registry')) {
             }
         }
     })  
+
+    // Temporarily disable the submit button to prevent multiple form submission
+    function disbleSendRequestBtn(btn) {
+        let oldValue = 'Yes'
+        btn.setAttribute('disabled', true)
+        btn.classList.replace('action-btn', 'disabled-btn')
+        btn.innerText = 'Sending'
+    
+        setTimeout(function(){
+            btn.innerText = oldValue;
+            btn.classList.replace('disabled-btn', 'action-btn')
+            btn.removeAttribute('disabled');
+        }, 5000)
+    }
 
     function closeModal(modal) {  
         let closeBtns = Array.from(document.getElementsByClassName('close-modal-btn'))
