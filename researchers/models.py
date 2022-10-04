febc5f90dc9cab33f6c6a079fa4d4dc2884ec414
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2022-10-04 16:25:12 -0700

    researcher description required #642#

--- researchers/models.py
+++ researchers/models.py
@@ -15,7 +15,7 @@ class Researcher(models.Model):
     image = models.ImageField(upload_to=researcher_img_path, blank=True, null=True)
     contact_email = models.EmailField(blank=True, null=True)
     contact_email_public = models.BooleanField(default=False, null=True, blank=True)
-    description = models.TextField(null=True, blank=True, validators=[MaxLengthValidator(200)])
+    description = models.TextField(null=True, validators=[MaxLengthValidator(200)])
     website = models.URLField(max_length=150, blank=True, null=True)
     primary_institution = models.CharField(max_length=250, null=True, blank=True)
     orcid_auth_token = models.TextField(null=True, blank=True)
