a41cf66077c6e17600dd9ac1ad68f2c5b20fdab0
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2022-10-04 15:12:54 -0700

    remove unused import

--- researchers/views.py
+++ researchers/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.postgres.search import SearchQuery, SearchVector, SearchRank
-from django.db.models import Q
 from django.contrib import messages
 from django.http import Http404
 from django.core.paginator import Paginator
