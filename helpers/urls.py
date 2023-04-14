efdfe252c608d01079963c72fe4f985c44c608aa
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2023-04-14 12:48:33 -0700

    adjust private projects

--- helpers/urls.py
+++ helpers/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('download/open-to-collaborate-notice/', views.download_open_collaborate_notice, name="download-open-to-collaborate-notice"),
-    path('restricted/', views.restricted_view, name="restricted"),
     path('invite/delete/<str:pk>/', views.delete_member_invite, name="delete-member-invite"),
 ]
