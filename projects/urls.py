bb94e35d0cd684ae21b22ddcf40fb505c6de3349
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2023-04-21 13:32:13 -0700

    move download project functionality to downloads file

--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('<uuid:unique_id>/', views.view_project, name="view-project"),
-    path('download/<uuid:unique_id>/', views.download_project_zip, name="download-project-zip"),
+    path('download/<uuid:unique_id>/', views.download_project, name="download-project-zip"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
-from .models import Project, ProjectContributors, ProjectCreator, ProjectPerson
+from .models import Project, ProjectContributors, ProjectCreator
 from helpers.models import Notice
-from helpers.utils import render_to_pdf, generate_zip
-from django.http import HttpResponse, Http404
-import requests
+from django.http import Http404
 from accounts.models import UserAffiliation
 from researchers.models import Researcher
+from helpers.downloads import download_project_zip
+from localcontexts.utils import dev_prod_or_local
 
 def view_project(request, unique_id):
     try:
@@ -59,122 +59,13 @@ def view_project(request, unique_id):
         return redirect('restricted')
 
 
-def download_project_zip(request, unique_id):
+def download_project(request, unique_id):
     try:
         project = Project.objects.prefetch_related('bc_labels', 'tk_labels').get(unique_id=unique_id)
 
-        if project.project_privacy == "Private":
+        if project.project_privacy == "Private" or dev_prod_or_local(request.get_host()) == 'DEV':
             return redirect('restricted')
         else:
-            baseURL = 'https://storage.googleapis.com/anth-ja77-local-contexts-8985.appspot.com/'
-            project_bclabels = project.bc_labels.all()
-            project_tklabels = project.tk_labels.all()
-            project_creator = ProjectCreator.objects.get(project=project)
-            contributors = ProjectContributors.objects.prefetch_related('communities', 'institutions', 'researchers').get(project=project)
-            project_people = ProjectPerson.objects.filter(project=project)
-
-            notice_exists = Notice.objects.filter(project=project).exists()
-
-            template_path = 'snippets/pdfs/project-pdf.html'
-            context = { 'project': project, 'project_creator': project_creator, 'contributors': contributors, 'project_people': project_people }
-
-            files = []
-
-            # Initialize README TEXT
-            readme_text = ''
-
-            # Set README text if both types of notice present
-            if notice_exists:
-                institution_text = "The Institution Notices are for use by collecting institutions, data repositories and organizations who engage in collaborative curation with Indigenous and other marginalized communities who have been traditionally excluded from processes of documentation and record keeping.\nThe Institution Notices are intended to be displayed prominently on public-facing institutional websites, on digital collections pages and or in finding aids."
-                notice_text = "The BC, TK and Attribution Incomplete Notices are specific tools for institutions and researchers which support the recognition of Indigenous interests in collections and data. The Notices are a mechanism for researchers and institutional staff to identify Indigenous collections and Indigenous interests in data.\n\nThe Notices can function as place-holders on collections, data, or in a sample field until a TK or a BC Label is added by a community."
-                readme_text = notice_text + '\n\n' + institution_text + '\n\nThis folder contains the following files:\n'
-
-            # Create PDF from project context, append to files list
-            pdf = render_to_pdf(template_path, context)
-            files.append(('Project_Overview.pdf', pdf))
-
-            # Label / Notice Files
-            if notice_exists:
-                notices = Notice.objects.filter(project=project)
-                for notice in notices:
-                    if not notice.archived:
-                        # Create PNG and TXT files based on which Notices are attached to the Project
-                        if notice.notice_type == 'biocultural':
-                            get_img = requests.get(notice.img_url)
-                            get_svg = requests.get(baseURL + 'labels/notices/bc-notice.svg')
-                            files.append(('Biocultural_Notice' + '.png', get_img.content))
-                            files.append(('Biocultural_Notice' + '.svg', get_svg.content))
-                            files.append(('Biocultural_Notice' + '.txt', notice.default_text))
-
-                        if notice.notice_type == 'traditional_knowledge':
-                            get_img = requests.get(notice.img_url)
-                            get_svg = requests.get(baseURL + 'labels/notices/tk-notice.svg')
-                            files.append(('Traditional_Knowledge_Notice' + '.png', get_img.content))
-                            files.append(('Traditional_Knowledge_Notice' + '.svg', get_svg.content))
-                            files.append(('Traditional_Knowledge_Notice' + '.txt', notice.default_text))
-                        
-                        if notice.notice_type == 'attribution_incomplete':
-                            get_img = requests.get(notice.img_url)
-                            get_svg = requests.get(baseURL + 'labels/notices/ci-attribution-incomplete.svg')
-                            files.append(('Attribution_Incomplete' + '.png', get_img.content))
-                            files.append(('Attribution_Incomplete' + '.svg', get_svg.content))
-                            files.append(('Attribution_Incomplete' + '.txt', notice.default_text))
-
-            if project_bclabels or project_tklabels:
-                # Set readme text
-                readme_text = "The Traditional Knowledge (TK) and Biocultural (BC) Labels reinforce the cultural authority and rights of Indigenous communities. \nThe TK and BC Labels are intended to be displayed prominently on public-facing Indigenous community, researcher and institutional websites, metadata and digital collection's pages.\n\nThis folder contains the following files:\n"
-
-            # Add Label images, text and translations
-            for bclabel in project_bclabels:
-                get_image = requests.get(bclabel.img_url)
-                get_svg = requests.get(bclabel.svg_url)
-                files.append((bclabel.name + '.png', get_image.content))
-                files.append((bclabel.name + '.svg', get_svg.content))
-
-                # Default Label text
-                text_content = bclabel.name + '\n' + bclabel.label_text
-                text_addon = []
-
-                if bclabel.bclabel_translation.all():
-                    for translation in bclabel.bclabel_translation.all():
-                        text_addon.append('\n\n' + translation.translated_name + ' (' + translation.language + ') ' + '\n' + translation.translated_text)
-                    files.append((bclabel.name + '.txt', text_content + '\n'.join(text_addon)))
-                else:
-                    files.append((bclabel.name + '.txt', text_content))
-
-            # Add Label images, text and translations
-            for tklabel in project_tklabels:
-                get_image = requests.get(tklabel.img_url)
-                get_svg = requests.get(tklabel.svg_url)
-                files.append((tklabel.name + '.png', get_image.content))
-                files.append((tklabel.name + '.svg', get_svg.content))
-                
-                # Default Label text
-                text_content = tklabel.name + '\n' + tklabel.label_text
-                text_addon = []
-
-                if tklabel.tklabel_translation.all():
-                    for translation in tklabel.tklabel_translation.all():
-                        text_addon.append('\n\n' + translation.translated_name + ' (' + translation.language + ') ' + '\n' + translation.translated_text)
-                    files.append((tklabel.name + '.txt', text_content + '\n'.join(text_addon)))
-                else:
-                    files.append((tklabel.name + '.txt', text_content))
-            
-            # Create Readme
-            file_names = []
-            for f in files:
-                file_names.append(f[0])
-            readme_content = readme_text + '\n'.join(file_names) + '\n\nRefer to the Usage Guides (https://localcontexts.org/support/downloadable-resources/) for details on how to adapt and display the Notices or Labels for your Project.\n\nFor more information, contact Local Contexts at localcontexts.org or support@localcontexts.org'
-            files.append(('README.txt', readme_content))
-
-            # Generate zip file 
-            full_zip_in_memory = generate_zip(files)
-
-            zipfile_name = f"LC-Project-{project.title}.zip"
-
-            response = HttpResponse(full_zip_in_memory, content_type='application/force-download')
-            response['Content-Disposition'] = 'attachment; filename="{}"'.format(zipfile_name)
-
-            return response
+            return download_project_zip(project)
     except:
         raise Http404()
