a9747bed11eb0f1ab5a8c3e73c52e33da5c947b1
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2023-06-26 11:32:27 -0700

    set up download tracking for collections care notices

--- helpers/urls.py
+++ helpers/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('download/open-to-collaborate-notice/<int:perm>/', views.download_open_collaborate_notice, name="download-open-to-collaborate-notice"),
-    path('download/collections-care-notices/<int:perm>/', views.download_collections_care_notices, name="download-collections-care-notices"),
+    path('download/collections-care-notices/<str:institution_id>/<int:perm>/', views.download_collections_care_notices, name="download-collections-care-notices"),
     path('invite/delete/<str:pk>/', views.delete_member_invite, name="delete-member-invite"),
     path('download/community/support-letter/', views.download_community_support_letter, name="download-community-support-letter"),
     path('download/institution/support-letter/', views.download_institution_support_letter, name="download-institution-support-letter"),
