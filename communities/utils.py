f9ac0ed491b9b4f6706b114b0a473b6374ab84dc
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2023-08-25 15:48:42 -0700

    set up label type validation to check if a label of this type has been created.

--- communities/utils.py
+++ communities/utils.py
@@ -3,6 +3,8 @@ from bclabels.utils import check_bclabel_type
 from tklabels.utils import check_tklabel_type
 from bclabels.forms import CustomizeBCLabelForm
 from tklabels.forms import CustomizeTKLabelForm
+from bclabels.models import BCLabel
+from tklabels.models import TKLabel
 
 def get_community(pk):
     return Community.objects.select_related('community_creator').prefetch_related('admins', 'editors', 'viewers').get(id=pk)
@@ -13,3 +15,10 @@ def get_form_and_label_type(label_code):
         'bc': (CustomizeBCLabelForm, "BC Label", check_bclabel_type(label_code))
     }
     return label_map.get(label_code[:2], (None, None, None))
+
+def does_label_type_exist(community, label_code):
+    label_type = check_tklabel_type(label_code) or check_bclabel_type(label_code)
+    if label_type:
+        return TKLabel.objects.filter(community=community, label_type=label_type).exists() or BCLabel.objects.filter(community=community, label_type=label_type).exists()
+    return False
+
