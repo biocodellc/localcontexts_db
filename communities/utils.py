8266164931f0793345cd50382a385a5fccb2d5b0
Diana Lovette <41635757+birbjam@users.noreply.github.com>
2023-08-25 16:10:34 -0700

    optimize Labels.json fetching and adjust checking if label type exists to accommodate similar types across Label models

--- communities/utils.py
+++ communities/utils.py
@@ -17,8 +17,14 @@ def get_form_and_label_type(label_code):
     return label_map.get(label_code[:2], (None, None, None))
 
 def does_label_type_exist(community, label_code):
-    label_type = check_tklabel_type(label_code) or check_bclabel_type(label_code)
-    if label_type:
-        return TKLabel.objects.filter(community=community, label_type=label_type).exists() or BCLabel.objects.filter(community=community, label_type=label_type).exists()
+    tk_label_type = check_tklabel_type(label_code)
+    bc_label_type = check_bclabel_type(label_code)
+    
+    if tk_label_type:
+        return TKLabel.objects.filter(community=community, label_type=tk_label_type).exists()
+    
+    if bc_label_type:
+        return BCLabel.objects.filter(community=community, label_type=bc_label_type).exists()
+
     return False
 
--- tklabels/utils.py
+++ tklabels/utils.py
@@ -2,13 +2,13 @@ from helpers.utils import get_labels_json
 
 # fetch data from Labels.json
 data = get_labels_json()
-
+                    
 def check_tklabel_type(label):
-    for key, values in data.items():
-        if key == 'tkLabels':
-            if(isinstance(values, list)):
-                for value in values:
-                    if label == value['labelCode']:
-                        return value['labelType']
-                    elif label == 'placeholder':
-                        return False
+    tk_labels = data.get('tkLabels', [])
+    
+    if label == 'placeholder':
+        return False
+
+    for value in tk_labels:
+        if label == value['labelCode']:
+            return value['labelType']
