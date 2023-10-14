f61395cbee9609ea3ab47cf58fb17656a7581a50
lc_hd <lc_hd>
2023-10-14 08:08:06 -0400

    feat: moved open modal code so it is only loaded on specific pages

--- localcontexts/static/javascript/main.js
+++ localcontexts/static/javascript/main.js
@@ -1267,31 +1267,6 @@ function copyToClipboard(elemID) {
     textArea.remove();
 }
 
-// Add member modal
-function openAddModalView() {
-    const inviteView = document.getElementById('inviteUserModalView')
-    const addView = document.getElementById('addMemberModalView')
-
-    // hide inviteView and show addView
-    addView.classList.replace('hide', 'show')
-    inviteView.classList.replace('show', 'hide')
-
-    // stop clicking on elements below
-    event.stopPropagation()
-}
-
-function openInviteUserModalView() {
-    const inviteView = document.getElementById('inviteUserModalView')
-    const addView = document.getElementById('addMemberModalView')
-
-    // hide addView and show inviteView
-    addView.classList.replace('show', 'hide')
-    inviteView.classList.replace('hide', 'show')
-
-    // stop clicking on elements below
-    event.stopPropagation()
-}
-
 function openMemberModal() {
     const memberModal = document.getElementById('memberModal')
     memberModal.classList.replace('hide', 'show')
@@ -1963,6 +1938,35 @@ if (window.location.href.includes('/institutions/update/') || window.location.hr
             e.preventDefault()
             modal.classList.add('hide')
         })
+    }
+
+ }
+
+ if (window.location.href.includes('communities/members/') ||  window.location.href.includes('institutions/members/')) {
+
+    // Add member modal
+    function openAddModalView() {
+        const inviteView = document.getElementById('inviteUserModalView')
+        const addView = document.getElementById('addMemberModalView')
+
+        // hide inviteView and show addView
+        addView.classList.replace('hide', 'show')
+        inviteView.classList.replace('show', 'hide')
+
+        // stop clicking on elements below
+        event.stopPropagation()
+    }
+
+    function openInviteUserModalView() {
+        const inviteView = document.getElementById('inviteUserModalView')
+        const addView = document.getElementById('addMemberModalView')
+
+        // hide addView and show inviteView
+        addView.classList.replace('show', 'hide')
+        inviteView.classList.replace('hide', 'show')
+
+        // stop clicking on elements below
+        event.stopPropagation()
     }
 
  }
