60d92e548e823c44a831e6b2223e81dbbb6e063e
Ashley Rojas <ashley.rojas95@gmail.com>
2023-10-20 12:57:23 -0400

    fix: projectID updated in embed code

--- localcontexts/static/javascript/main.js
+++ localcontexts/static/javascript/main.js
@@ -1580,8 +1580,9 @@ if (window.location.href.includes('projects')) {
     var languageDropdown = document.getElementById('embedLanguageOptions')
     var langArray= new Array();
     var layoutType, languageType, customizationOptions = null
+    projectID = embedCode.dataset.projectId
     
-    embedCode.value = '<iframe width="560" height="250" src="http://'+window.location.host+'/projects/embed/f5850e3d-931e-46c0-9cb1-9969a4672271/" title="Local Contexts Project Identifiers" frameborder="0"></iframe>'
+    embedCode.value = '<iframe width="560" height="250" src="http://' + window.location.host + '/projects/embed/' + projectID + '/" title="Local Contexts Project Identifiers" frameborder="0"></iframe>'
 
     for (i=0;i < languageDropdown.options.length; i++) {
         if (langArray.includes(languageDropdown.options[i].value) == false) {
@@ -1617,7 +1618,7 @@ if (window.location.href.includes('projects')) {
             customizationOptions = languageType
         }
 
-        embedCode.value = '<iframe width="560" height="250" src="http://'+window.location.host+'/projects/embed/f5850e3d-931e-46c0-9cb1-9969a4672271?'+customizationOptions+'" title="Local Contexts Project Identifiers" frameborder="0"></iframe>'
+        embedCode.value = '<iframe width="560" height="250" src="http://' + window.location.host + '/projects/embed/' + projectID + '?' + customizationOptions + '" title="Local Contexts Project Identifiers" frameborder="0"></iframe>'
     }
 }
 
